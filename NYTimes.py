30977410d6482895df16e3b5cd93050365946b36
mgenson <46972714+mgenson@users.noreply.github.com>
2019-04-18 17:44:22 -0400

    working on getting database to work

--- NYTimes.py
+++ NYTimes.py
@@ -14,30 +14,30 @@ def scrape_nyt_politics():
     
     obama = obama_dict["response"]['docs'][0]
     trump = trump_dict["response"]["docs"][0]
-    print("BUSH", bush,"OBAMA", obama,"TRUMP", trump)
-    # Make bush json
-    dumped_json_bush = json.dumps(bush)
-    bw = open("bush_json","w")
-    bw.write(dumped_json_bush)
-    bw.close() 
-    #make obama json
-    dumped_json_obama = json.dumps(obama)
-    ow = open("obama_json", "w")
-    ow.write(dumped_json_obama)
-    ow.close
-    #make trump json
-    dumped_json_trump = json.dumps(trump)
-    tw = open
-    tw.write("trump_json", "w")
-    tw.close()
-#def data_nyt(dumped_json_bush, dumped_json_obama, dumped_json_trump):   
+  
+   
+def data_nyt(bush, obama, trump):   
 
     #make connection to database
-    #conn = sqlite3.connect("NYT.sqlite")
-    #cur = conn.cursor()
-    #cur.execute("CREATE TABLE IF DOESN'T EXIST NYT")
-    #cur.execute("CREATE TABLE NYT(")
-#def visual_nyt()
+    conn = sqlite3.connect("NYT.sqlite")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF DOESN'T EXIST NYT")
+    cur.execute("CREATE TABLE NYT(url Text, headline TEXT, date INTEGER, source, TEXT )")
+    # use a loop, the defined cursor, to execute INSERT statements, 
+    #that insert the data from each admin into the correct columns in 
+    # each row of the NYT database table
+    sql = "INSERT INTO NYT (url, headline, date, source) VALUES (?, ?, ?, ?)"
+    for bush in bush:
+        bushData =(bush["web_url"], bush["headline"], bush["pub_date"], bush["source"])
+        cur.execute(sql, bushData)    
+    for obama in obama:
+        obamaData =(obama["web_url"], obama["headline"],  obama["pub_date"], obama["source"])
+        cur.execute(sql, obamaData)
+    for trump in trump:
+        trumpData = (trump['web_url'], trump["headline"], trump["pub_date"], trump["source"])
+        cur.execute(sql, trumpData)
+    conn.commit()
+#def visual_nyt(NYT)'''
     
     
 if __name__ == '__main__':
