ea2ea3de9cd36bd54072f6e33786e1af8539df9f
Eva Smith <Eva@0587373751.wireless.umich.net>
2019-04-18 14:49:04 -0400

    Code to scrape data working; still need to check information for duplicates before commiting to table

--- news.py
+++ news.py
@@ -6,15 +6,44 @@ from news_info import news_api
 from newsapi import NewsApiClient
 import sqlite3
 
-
 def news_scrape():
     api = NewsApiClient(api_key = news_api)
 
     conn = sqlite3.connect('Eva_test.sqlite')
     cur = conn.cursor()
 
-    #cur.execute('CREATE TABLE IF NOT EXISTS News()')
-    requests = api.get_everything(q='politics') #returns dictionary
+    cur.execute('CREATE TABLE IF NOT EXISTS News(id TEXT, news_outlet TEXT, author TEXT, title TEXT, description TEXT, url TEXT, publishedAt TIMESTAMP, content TEXT)')
+    sql = "INSERT INTO News (id, news_outlet, author, title, description, url, publishedAt, content) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+    articles = api.get_everything(q='politics') #returns dictionary
+    print(articles)
+    num = 0
+    for article in articles['articles']:
+        if num >= 20:
+            break
+        else:
+            news_id = article['source']['id']
+            news_outlet = article['source']['name']
+            news_author = article['author']
+            news_title = article['title']
+            news_description = article['description']
+            news_url = article['url']
+            news_publishedAt = article['publishedAt']
+            news_content = article['content']
+        num += 1
+            # cur.execute('SELECT title FROM News WHERE title =') How to check that this doesn't equal one in table already?
+            # if title is new:
+        val = (news_id, news_outlet , news_author, news_title, news_description, news_url, news_publishedAt, news_content)
+        cur.execute(sql, val)
+	#  Use the database connection to commit the changes to the database
+        conn.commit()
+    
+def news_vis():
+    # analyzing sentiment of text;
+    # compares from different sources or different search terms
+    # cur.execute("SELECT * FROM news") # for news visual
+    # pass content string into sentiment analyzer
+    # graph sentiment of each article somehow
+    pass
 
 if __name__ == "__main__":
     news_scrape()
