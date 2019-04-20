a9a7285a83b979a686e68463ace3b77d5a89018c
Eva Smith <Eva@Evas-MacBook-Pro.local>
2019-04-20 13:31:58 -0400

    Code is collecting data and populating tables without duplicates, taking nomore than 20 per time.

--- news.py
+++ news.py
@@ -9,18 +9,21 @@ import sqlite3
 def news_scrape():
     api = NewsApiClient(api_key = news_api)
 
-    conn = sqlite3.connect('Eva_test2.sqlite')
+    conn = sqlite3.connect('Eva-test.sqlite')
     cur = conn.cursor()
     
-    cur.execute('CREATE TABLE IF NOT EXISTS News2(news_outlet TEXT, author TEXT, title TEXT, description TEXT, url TEXT, publishedAt TIMESTAMP, content TEXT, UNIQUE (title))')
-    sql = "INSERT INTO News (news_outlet, author, title, description, url, publishedAt, content) VALUES (?, ?, ?, ?, ?, ?, ?)"
-    articles = api.get_everything(q='politics') #returns dictionary
+    cur.execute('CREATE TABLE IF NOT EXISTS Breaking(news_outlet TEXT, author TEXT, title TEXT, description TEXT, url TEXT, publishedAt TIMESTAMP, content TEXT, UNIQUE (news_outlet, author, title, description, url, publishedAt, content))')
+    
+    articles = api.get_everything(q='breaking', from_param='2019-03-20',
+                                      to='2019-04-22') #returns dictionary
     
     num = 0
     for article in articles['articles']:
         if num >= 20:
             break
         else:
+            sql = "INSERT OR IGNORE INTO Breaking(news_outlet, author, title, description, url, publishedAt, content) VALUES (?, ?, ?, ?, ?, ?, ?)"
+            # news_info = cur.fetchone()
             news_outlet = article['source']['name']
             news_author = article['author']
             news_title = article['title']
@@ -28,12 +31,12 @@ def news_scrape():
             news_url = article['url']
             news_publishedAt = article['publishedAt']
             news_content = article['content']
-            num += 1
-            # cur.execute('SELECT title FROM News WHERE title =') How to check that this doesn't equal one in table already?
-            # if title is new:
+        num += 1
+                # cur.execute('SELECT title FROM News WHERE title =') How to check that this doesn't equal one in table already?
+                # if title is new:
         val = (news_outlet , news_author, news_title, news_description, news_url, news_publishedAt, news_content)
         cur.execute(sql, val)
-	#  Use the database connection to commit the changes to the database
+        #  Use the database connection to commit the changes to the database
         conn.commit()
     
 def news_vis():
