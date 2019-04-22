14e5d9848e01423156d4bd65e456ea89723a5ecb
Eva Smith <Eva@Evas-MBP.lan>
2019-04-22 14:12:56 -0400

    News table is no longer gathering duplicates; Final.sqlite updated

--- news.py
+++ news.py
@@ -12,19 +12,17 @@ def news_scrape():
     conn = sqlite3.connect('Final.sqlite')
     cur = conn.cursor()
     
-    cur.execute('CREATE TABLE IF NOT EXISTS News(news_outlet TEXT, author TEXT, title TEXT, description TEXT, url TEXT, publishedAt TIMESTAMP, content TEXT, UNIQUE (news_outlet, author, title, description, url, publishedAt, content))')
+    cur.execute('CREATE TABLE IF NOT EXISTS News(news_outlet TEXT, author TEXT, title TEXT, description TEXT, url TEXT, publishedAt TIMESTAMP, content TEXT)')
     
-    articles = api.get_everything(q='politics', from_param='2019-03-21',
-                                      to='2019-04-22') #returns dictionary
+    articles = api.get_everything(q='politics', from_param='2019-03-22',
+                                      to='2019-04-23') #returns dictionary
     
     num = 0
-    num_added = 0
+    
     for article in articles['articles']:
         if num >= 20:
             break
         else:
-            sql = "INSERT OR IGNORE INTO News(news_outlet, author, title, description, url, publishedAt, content) VALUES (?, ?, ?, ?, ?, ?, ?)"
-            # news_info = cur.fetchone()
             news_outlet = article['source']['name']
             news_author = article['author']
             news_title = article['title']
@@ -34,12 +32,14 @@ def news_scrape():
             news_content = article['content']
         num += 1
     
-                # cur.execute('SELECT title FROM News WHERE title =') How to check that this doesn't equal one in table already?
-                # if title is new:
-        val = (news_outlet , news_author, news_title, news_description, news_url, news_publishedAt, news_content)
-        cur.execute(sql, val)
+        cur.execute('SELECT * FROM News WHERE title = "%s"' % news_title) 
+        row_info = cur.fetchone()
+        if row_info == None:
+            sql = "INSERT OR IGNORE INTO News(news_outlet, author, title, description, url, publishedAt, content) VALUES (?, ?, ?, ?, ?, ?, ?)"
+            val = (news_outlet , news_author, news_title, news_description, news_url, news_publishedAt, news_content)
+            cur.execute(sql, val)
         #  Use the database connection to commit the changes to the database
-        conn.commit()
+    conn.commit()
 
 
 if __name__ == "__main__":
